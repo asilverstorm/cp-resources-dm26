308d51a3ad23f8626a41acfec62d5d779db6a022
Silver Storm <78556714+asilverstorm@users.noreply.github.com>
2022-12-09 11:41:18 -0500

    code optimization + audits introduced
    praise be to chatgpt

--- scripts/hax4win.bat
+++ scripts/hax4win.bat
@@ -4,27 +4,28 @@ title CyberPatriot Starting Script - RUN AS ADMIN
 
 rem Variables
 
-net session>nul
-if %errorlevel%==0 (
-	echo Admin rights granted!
-    timeout /t 1 /NOBREAK>nul
-    cls
-) else (
-    echo Failure, no rights
-    timeout /t 1 /NOBREAK>nul
-    exit
-)
-
-echo confirm you have completed the forensic questions (y/n)
-set /p forensics=
-    if /I %forensics%==y (
-        goto :mainMenu
+rem net session>nul
+    if %errorlevel%==0 (
+	    echo Admin rights granted!
+        timeout /t 1 /NOBREAK>nul
+        cls
     ) else (
-        echo then go answer them dummy
-        timeout /t 2 /NOBREAK>nul
+        echo Failure, no rights
+        timeout /t 1 /NOBREAK>nul
         exit
     )
-    
+
+:forensicsConfirm
+    echo confirm you have completed the forensic questions (y/n)
+    set /p forensics=
+        if /I %forensics%==y (
+            goto :mainMenu
+        ) else (
+            echo then go answer them dummy
+                timeout /t 2 /NOBREAK>nul
+        exit
+    )
+
 :mainMenu
     cls
     echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -48,60 +49,32 @@ set /p forensics=
 	echo 07} security policies       08} enable defender/firewall
 	echo 09} search files            10} disable services
 	echo 11}                         12} RDS configuration
-	echo 13}                         14} launch checklist w/ notes
+	echo 13} save                    14} launch checklist w/ notes
 	echo 15} full scan cachine       16} edit groups
-    echo 17} set audits              18} check ports/netcat
+    echo 17} set audit policies      18} check ports/netcat
 	echo 19} view changes            20} exit
     echo [91m66} execute order 66 [0m
     echo ~
 
     set /p navigate=select--
-
-rem Table Of Contents
-rem possible to condense in 2 lines? both navigation inputs and outputs into strings/tables
-        if %navigate%==1 ( goto :createUser )
-        if %navigate%==01 ( goto :createUser )
-        if %navigate%==2 ( goto :removeUser )
-        if %navigate%==02 ( goto :removeUser )
-        if %navigate%==3 ( goto :placeHolder )
-        if %navigate%==03 ( goto :placeHolder )
-        if %navigate%==4 ( goto :placeHolder )
-        if %navigate%==04 ( goto :placeHolder )
-        if %navigate%==5 ( goto :placeHolder )
-        if %navigate%==05 ( goto :placeHolder )
-        if %navigate%==6 ( goto :placeHolder )
-        if %navigate%==06 ( goto :placeHolder )
-        if %navigate%==7 ( goto :placeHolder )
-        if %navigate%==07 ( goto :placeHolder )
-        if %navigate%==8 ( goto :placeHolder )
-        if %navigate%==08 ( goto :placeHolder )
-        if %navigate%==9 ( goto :placeHolder )
-        if %navigate%==09 ( goto :placeHolder )
-        if %navigate%==10 ( goto :placeHolder )
-        if %navigate%==11 ( goto :placeHolder )
-        if %navigate%==12 ( goto :placeHolder )
-        if %navigate%==13 ( goto :placeHolder )
-        if %navigate%==14 ( goto :placeHolder )
-        if %navigate%==15 ( goto :placeHolder )
-        if %navigate%==16 ( goto :placeHolder )
-        if %navigate%==17 ( goto :placeHolder )
-        if %navigate%==18 ( goto :placeHolder )
-        if %navigate%==19 ( goto :placeHolder )
-        if %navigate%==20 ( goto :exit )
-
-        echo error 404, directory not found, unknown input
-        echo press any key to reset. . . 
-        pause>nul
-           goto :mainMenu
-
-rem Functions - AUDITS NEEDED
-
-:placeHolder
-    echo %navigate% registered, page unavailable, still in development, press any key to reset. . .
+    for /l %%i in (1,1,20) do (
+        if "!navigate!"=="0%%i" (
+            goto :menuOption%%i
+        )
+        if "!navigate!"=="%%i" (
+            goto :menuOption%%i
+        )
+    )
+    echo error 404, directory not found, unknown input
+    echo [91m!%navigate%[0m registered, page may be unavailable
+    echo press any key to reset. . . 
     pause>nul
-    goto:mainMenu
+    goto :mainMenu
 
-:createUser
+
+
+rem create user
+:menuOption1 
     cls
 
     rem Backs out in case of mistake
@@ -115,18 +88,27 @@ rem Functions - AUDITS NEEDED
     set /p userAdd=
     net user !userAdd! /add
 
-    rem Runs a fail check
+    rem Runs a fail check, may be redundant/unnecessary 
     net user %userAdd%>nul
+        
+        if not defined userAdd (
+            echo Error: no user name provided. Press any key to return to menu. . .
+            pause>nul
+                goto :mainMenu
+        )        
+        
         if %ERRORLEVEL%==0 (goto :continueAdd) else (
             echo [91mfailure to add user, press any key to return to menu. . .[0m
             pause>nul
                 goto :mainMenu
         )
 
+
     :continueAdd
         echo user [92m!userAdd![0m created, revert changes? (y/n)
         set /p revert1=
             if %revert1%==n (
+                echo %date% %time% - %userAdd% was created >> audit.txt
                 echo action confirmed, press any key to return to menu. . .
                 pause>nul
                     goto :mainMenu
@@ -148,7 +130,7 @@ rem Functions - AUDITS NEEDED
             )
 
 
-:removeUser
+:menuOption2
     cls
 
     rem Backs out in case of mistake
@@ -157,12 +139,19 @@ rem Functions - AUDITS NEEDED
         if %confirm2%==n (goto :mainMenu)
 
     rem Actually removes user (after input)
-    echo removing a user...
-    echo please input name of unlucky user:
+    net user
+    echo please choose a user to be removed:
     set /p userDel=
     net user !userDel! /delete
 
     rem Runs a fail check
+
+    if not defined userAdd (
+                echo Error: no user name provided. Press any key to return to menu. . .
+                pause>nul
+                    goto :mainMenu
+            ) 
+
     net user %userDel%>nul
         if %ERRORLEVEL%==0 (
             echo [91mfailure to remove user, press any key to return to menu. . .[0m
@@ -176,6 +165,7 @@ rem Functions - AUDITS NEEDED
         echo user [92m!userDel![0m removed, revert changes? (y/n)
         set /p revert2=
             if %revert2%==n (
+                echo %date% %time% - %userDel% was deleted >> audit.txt
                 echo action confirmed, press any key to return to menu. . .
                 pause>nul
                     goto :mainMenu
@@ -197,9 +187,31 @@ rem Functions - AUDITS NEEDED
                     goto :mainMenu
             )
 
+
+:order66
+    cls 
+    echo [91mare you sure you want to do this..? (y/n)[0m
+    set /p confirm66=
+        if %confirm66%==y (
+            echo input passphrase to start killing jedi kids
+            set /p execute=%
+                if %execute%==
+            rem execute
+            echo review/revert changes? (y/n)
+            set /p anakinregret=
+                if %anakinregret%==y (
+                    goto :changelog
+                )
+            
+    ) else (
+        goto :mainmenu
+    )
     
-        
-        
+    
+
+:audit
+
+
 :exit
     echo are you sure you want to exit the script? (y/n)
     set /p confirm20=
@@ -207,3 +219,4 @@ rem Functions - AUDITS NEEDED
     echo why didnt you just press the x and close the application...
     timeout /t 2 /NOBREAK>nul
     exit
+
